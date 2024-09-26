882eeed7ea7dc5fcc5a401aae2b374ec8d661945
Ulviyya Alakbarova <ulviyyalakbarova@gmail.com>
2024-09-27 01:38:55 +0400

    new commit

--- src/main/java/az/atl/academy/CmsSecurityApplication.java
+++ src/main/java/az/atl/academy/CmsSecurityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CmsSecurityApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(CmsSecurityApplication.class, args);
 	}
 
